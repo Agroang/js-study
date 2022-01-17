a33d32757694e57fba4efe31e23417b272b3d4c1
matias acuna <agroang@gmail.com>
2022-01-17 15:52:13 +0900

    added ES6 destructuring explanation and examples

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // JavaScript: From Fundamentals to Functional JS, v2
 // https://slides.com/bgando/f2f-final-day-1
 // https://slides.com/bgando/f2f-final-day-2
+
+// ES6 Destructuring: (objects, so Arrays & Objects)
+// The destructuring assignment syntax is a JavaScript expression that makes it
+// possible to unpack values from arrays, or properties from objects, into
+// distinct variables.
+// You have the "target" to the left and the "source" to the right
+// Examples:
+// (variable declaration array & object)
+// const [first, second] = [true, false]
+// const {first, second} = {first: 0, second: 1}
+// first has a value of true, and second of false, then 0 and 1 in the object
+// example
+// (assignment array & object)
+// [first, second] = [true, false]
+// {first, second} = {first: 0, second: 1}
