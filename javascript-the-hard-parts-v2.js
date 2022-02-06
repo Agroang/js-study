66e52f46bc91d96c151edfe05d2ea0d9831b24be
matias acuna <agroang@gmail.com>
2022-02-06 14:54:05 +0900

    started second course about hard parts of JS, added information about the thread of execution, memory and the call stack in JS

---
+++ javascript-the-hard-parts-v2.js
@@ -0,0 +1,13 @@
+// https://frontendmasters.com/courses/javascript-hard-parts-v2/
+// https://static.frontendmasters.com/resources/2019-09-18-javascript-hard-parts-v2/javascript-hard-parts-v2.pdf
+
+// The Three JavaScript Principles:
+// 1) Thread of Execution: JS runs code line by line
+// 2) Memory: JS saves 'data' in it's memory for us to use later
+// 3) Call Stack: JS keeps track of what is currently running
+
+// When you run a function it gets added to the stack, once you hit return
+// you go back one level above, the outermost level is global()
+
+// There is an "Execution Context" created to run the code of a function
+// and it has two parts: the thread of execution and the memory
